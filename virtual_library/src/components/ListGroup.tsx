2d680e9790ddb8dca8f40eb8255915697defc9fe
Tobias Fremming <tobif@stud.ntnu.no>
2024-05-25 18:56:57 +0200

    feat: handling events

--- virtual_library/src/components/ListGroup.tsx
+++ virtual_library/src/components/ListGroup.tsx
@@ -1,15 +1,17 @@
 function ListGroup() {
   const items = ["apples", "bananas", "oranges", "grapes", "pears"];
 
+  const handleClick = (index: number, item: string) => console.log(index, item);
+
   return (
     <>
       <h1>Title</h1>
       {items.length === 0 && <p>No items to display</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
             className="list-group-item"
-            onClick={() => console.log(item)}
+            onClick={handleClick.bind(null, index, item)}
             key={item}
           >
             {item}
