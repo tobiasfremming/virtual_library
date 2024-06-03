df2b98542bc8fb5349590be1ffa47ecb5b91b9dd
Tobias Fremming <tobif@stud.ntnu.no>
2024-06-03 20:22:28 +0200

    refactor: Update ExpenseFilter component to import categories from separate module

--- virtual_library/src/components/Expense/ExpenseFilter.tsx
+++ virtual_library/src/components/Expense/ExpenseFilter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { categories } from "../../App";
+import categories from "../../categories";
 
 interface ExpenseFilterProps {
   onSelectCategory: (category: string) => void;
