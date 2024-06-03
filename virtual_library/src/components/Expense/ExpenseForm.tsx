31ef2d0eba2c09cbee429cbca842b6e782d86a5b
Tobias Fremming <tobif@stud.ntnu.no>
2024-06-03 19:34:10 +0200

    refactor: Update ExpenseForm component to include category selection

--- virtual_library/src/components/Expense/ExpenseForm.tsx
+++ virtual_library/src/components/Expense/ExpenseForm.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { categories } from "../../App";
 
 const ExpenseForm = () => {
   return (
@@ -11,16 +12,23 @@ const ExpenseForm = () => {
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
-          amount
+          Amount
         </label>
-        <input id="amount" type="number" className="form-controll" />
+        <input id="amount" type="number" className="form-control" />
       </div>
+
       <div className="mb-3">
         <label htmlFor="category" className="form-label"></label>
         <select id="category" className="form-select">
-          <option value=""></option>
+          <option value="">Select category</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </div>
+      <button className="btn btn-primary">Add</button>
     </form>
   );
 };
