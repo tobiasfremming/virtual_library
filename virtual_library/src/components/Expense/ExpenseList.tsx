b200f7458702d66bea1262d73153ddb6ddf3cac5
Tobias Fremming <tobif@stud.ntnu.no>
2024-06-03 19:18:20 +0200

    refactor: Add ExpenseList component to display and manage expenses

---
+++ virtual_library/src/components/Expense/ExpenseList.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+
+interface ExpenseListProps {
+  expenses: Expense[];
+  onDelete: (id: number) => void;
+}
+
+const ExpenseList = ({ expenses, onDelete }: ExpenseListProps) => {
+  if (expenses.length === 0) {
+    return <p>No expenses</p>;
+  }
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expense) => (
+          <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
+            <td>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => onDelete(expense.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>
+            NOK
+            {expenses
+              .reduce((acc, expense) => expense.amount + acc, 0)
+              .toFixed(2)}
+          </td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
+    </table>
+  );
+};
+
+export default ExpenseList;
